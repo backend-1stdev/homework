a5dece745bdd3589a0e2fdce384501bbb14bea63
backend-1stdev <backend@1stdev.ru>
2021-06-03 20:37:14 +0300

    Home Work - 1

--- homework/Program.cs
+++ homework/Program.cs
@@ -2,14 +2,46 @@
 
 namespace homework
 {
+    abstract class Delivery 
+    {
+        public string Address;
+    }
+
+    class HomeDelivery: Delivery 
+    {
+  
+    }
+
+    class PickPointDelivery: Delivery 
+    {
+  
+    }
+
+    class ShopDelivery: Delivery 
+    {
+
+    }
+
+    class Order < TDelivery,
+        TStruct > where TDelivery: Delivery 
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress() 
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-            
-            fgfg
         }
     }
 }
